5f5b4f9be58ec5153941facc260f1ef03a0334a0
azouz34200 <mickael.azema@gmail.com>
2017-03-08 21:10:11 +0100

    delete aws provider variable

--- global/variables.tf
+++ global/variables.tf
@@ -4,7 +4,5 @@ variable "group-name" {}
 variable "ssh_pubkey_file" {}
 # Configure the AWS Provider
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
     region = "eu-west-1"
 }
